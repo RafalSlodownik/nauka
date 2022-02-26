d2b6fc320f3d0e8f437f74764a51e62cde8b237d
“RafalSlodownik” <rafal.slodownik@gmail.com>
2022-02-26 12:45:13 +0100

    exercises 4,5

---
+++ zadania/strings/strings_4.py
@@ -0,0 +1,2 @@
+random_word = input("Podaj dowolne słowo: ")
+print(random_word.upper())
---
+++ zadania/strings/strings_5.py
@@ -0,0 +1,3 @@
+random_sentence = input("Podaj napis z pięcioma białymi znakami na początku: ")
+print(random_sentence)
+print(random_sentence.lstrip())
